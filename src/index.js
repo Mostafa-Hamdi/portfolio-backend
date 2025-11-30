31490cc5cfb99209781c3250985295338ab8d8e7
Mostafa-Hamdi <mostafahamdi484@gmail.com>
2025-11-30 14:06:13 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const cors = require("cors");
-
 const express = require("express");
 require("./db/mongoose.js");
+
 const errorHandler = require("./middleware/errorHandler.js");
 const subscriperRouter = require("./routers/subscriber.js");
 const userRouter = require("./routers/user.js");
@@ -10,15 +10,17 @@ const projectRouter = require("./routers/project.js");
 const landingRouter = require("./routers/landing.js");
 const serviceRouter = require("./routers/service.js");
 const experienceRouter = require("./routers/experience.js");
+
 const app = express();
 
+// Middleware
 app.use(
-  cors({ origin: "https://mostafadev-gamma.vercel.app", credentials: true }),
+  cors({ origin: "https://mostafahamdidev.vercel.app", credentials: true }),
 );
-app.use(express.static(path.join(__dirname, "./assets")));
-const port = process.env.PORT || 3001;
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./assets")));
 
+// Routers
 app.use(
   subscriperRouter,
   userRouter,
@@ -28,9 +30,11 @@ app.use(
   experienceRouter,
 );
 
-// errorHandler middleware
+// Error handler
 app.use(errorHandler);
 
+// Start server
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log("Web Server Starts Up");
+  console.log(`Web Server running on port ${port}`);
 });
